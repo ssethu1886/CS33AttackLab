fa60f9c2256723191ebf39f91a79f0283764fb63
ssethu1886 <125099518+ssethu1886@users.noreply.github.com>
2023-05-22 22:28:10 -0700

    Add files via upload

---
+++ farm.c
@@ -0,0 +1,223 @@
+/* This function marks the start of the farm */
+int start_farm()
+{
+    return 1;
+}
+
+unsigned addval_413(unsigned x)
+{
+    return x + 3347663063U;
+}
+
+unsigned getval_469()
+{
+    return 2496104776U;
+}
+
+unsigned getval_154()
+{
+    return 2421731082U;
+}
+
+void setval_127(unsigned *p)
+{
+    *p = 2462550344U;
+}
+
+void setval_405(unsigned *p)
+{
+    *p = 2417531550U;
+}
+
+void setval_121(unsigned *p)
+{
+    *p = 3281031256U;
+}
+
+unsigned addval_242(unsigned x)
+{
+    return x + 3281031192U;
+}
+
+void setval_175(unsigned *p)
+{
+    *p = 3284633928U;
+}
+
+/* This function marks the middle of the farm */
+int mid_farm()
+{
+    return 1;
+}
+
+/* Add two arguments */
+long add_xy(long x, long y)
+{
+    return x+y;
+}
+
+void setval_370(unsigned *p)
+{
+    *p = 3378565513U;
+}
+
+unsigned addval_457(unsigned x)
+{
+    return x + 3251538417U;
+}
+
+void setval_451(unsigned *p)
+{
+    *p = 3374370441U;
+}
+
+unsigned getval_449()
+{
+    return 3677929993U;
+}
+
+void setval_211(unsigned *p)
+{
+    *p = 3674787337U;
+}
+
+unsigned addval_189(unsigned x)
+{
+    return x + 3768141924U;
+}
+
+void setval_246(unsigned *p)
+{
+    *p = 3281047177U;
+}
+
+unsigned getval_281()
+{
+    return 2497743176U;
+}
+
+unsigned addval_486(unsigned x)
+{
+    return x + 2430634312U;
+}
+
+unsigned addval_264(unsigned x)
+{
+    return x + 3372794505U;
+}
+
+unsigned getval_484()
+{
+    return 3767093271U;
+}
+
+unsigned getval_224()
+{
+    return 2430642504U;
+}
+
+unsigned addval_332(unsigned x)
+{
+    return x + 3378565513U;
+}
+
+unsigned getval_375()
+{
+    return 3383018121U;
+}
+
+unsigned addval_218(unsigned x)
+{
+    return x + 4190290569U;
+}
+
+unsigned addval_415(unsigned x)
+{
+    return x + 3676885385U;
+}
+
+unsigned getval_397()
+{
+    return 3286272330U;
+}
+
+unsigned addval_422(unsigned x)
+{
+    return x + 3229929865U;
+}
+
+unsigned addval_344(unsigned x)
+{
+    return x + 3281047937U;
+}
+
+unsigned getval_265()
+{
+    return 1086571145U;
+}
+
+unsigned addval_226(unsigned x)
+{
+    return x + 3286273352U;
+}
+
+void setval_380(unsigned *p)
+{
+    *p = 3375415945U;
+}
+
+void setval_117(unsigned *p)
+{
+    *p = 3677933961U;
+}
+
+unsigned getval_103()
+{
+    return 3380924045U;
+}
+
+void setval_216(unsigned *p)
+{
+    *p = 3674784457U;
+}
+
+unsigned addval_462(unsigned x)
+{
+    return x + 2915291545U;
+}
+
+unsigned getval_342()
+{
+    return 3675836041U;
+}
+
+unsigned getval_270()
+{
+    return 3268512151U;
+}
+
+unsigned getval_472()
+{
+    return 2445445471U;
+}
+
+void setval_419(unsigned *p)
+{
+    *p = 3534016137U;
+}
+
+unsigned addval_305(unsigned x)
+{
+    return x + 3465280065U;
+}
+
+unsigned getval_176()
+{
+    return 3269495112U;
+}
+
+/* This function marks the end of the farm */
+int end_farm()
+{
+    return 1;
+}
